f76f1d7cad02c111ff12f54311ac35558e3f2413
Raphael <125095194+Raphaeldev84@users.noreply.github.com>
2023-12-09 21:05:50 -0300

    feat: add rota vacina aplicada

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const router = Router();
 
 const pacienteRouters = require("./paciente");
 const vacinaRouters = require("./vacina");
+const vacinaAplicadaRouters = require("./vacinaAplicada");
 
 router.use("/paciente", pacienteRouters)
 router.use("/vacina", vacinaRouters)
+router.use("/vacinaaplicada", vacinaAplicadaRouters);
 
 module.exports = router
